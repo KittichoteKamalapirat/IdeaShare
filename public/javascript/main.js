4425e105534dd03be8e844b25af2dfe0e2880587
KittiShane <kittichotekamalapirat@Kittichotes-Air.hsd1.ca.comcast.net>
2019-06-14 15:58:57 -0700

    modal box done

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,12 @@
-function loginUser() {
-  window.localStorage.setItem('x-auth-token', token);
-}
+// Open the register modal
+document
+  .getElementById('register-button')
+  .addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+  });
 
-//   }
-//   );
+// close the register modal
+console.log(document.querySelector('.close'));
+document.querySelector('.close').addEventListener('click', function() {
+  document.querySelector('.bg-modal').style.display = 'none';
+});
